8eb1c7fc684840014226bc1fa77f31582ffda886
Gustavo Frederico <gustavo@swingby.network>
2021-01-15 10:34:49 -0500

    Changing the copyright comment in new file. No functional change.

--- ecdsa/signing/key_derivation_test.go
+++ ecdsa/signing/key_derivation_test.go
@@ -1,8 +1,4 @@
-// Copyright © 2019 Binance
-//
-// This file is part of Binance. The full Binance copyright notice, including
-// terms governing use, modification, and redistribution, is contained in the
-// file LICENSE at the root of the source code distribution tree.
+// Copyright © 2021 Swingby
 
 package signing
 
