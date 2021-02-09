04ca78dae76c9d4afdb71d8e5e780689bd6b7c8c
Gustavo Frederico <gustavo@swingby.network>
2021-02-08 20:20:51 -0500

    Signing optimizations - eddsa signing - resetting test updater function to original one

--- eddsa/signing/local_party_test.go
+++ eddsa/signing/local_party_test.go
@@ -56,7 +56,7 @@ func TestE2EConcurrent(t *testing.T) {
 	outCh := make(chan tss.Message, len(signPIDs))
 	endCh := make(chan *SignatureData, len(signPIDs))
 
-	updater := test.SharedPartyUpdaterWithQueues
+	updater := test.SharedPartyUpdater
 
 	msg := big.NewInt(200)
 	// init the parties
