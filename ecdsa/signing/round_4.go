710c17335a084f0f77f3025a60d56dcbb7a654c9
Gustavo Frederico <gustavo@swingby.network>
2021-02-25 12:46:34 -0500

    Signing optimizations - minor optimization between rounds 4 and 5. Simplifying small redundant code fragment.

--- ecdsa/signing/round_4.go
+++ ecdsa/signing/round_4.go
@@ -42,7 +42,7 @@ func (round *round4) CanProcess(msg tss.ParsedMessage) bool {
 }
 
 func (round *round4) CanProceed() bool {
-	return round.started && round.ended
+	return round.started
 }
 
 func (round *round4) NextRound() tss.Round {
