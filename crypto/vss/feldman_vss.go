b396352ce981d16ec441eec6d02e56931f1b590b
Gustavo Frederico <gustavo@swingby.network>
2021-10-27 19:26:33 -0400

    Bug fix for vss, check shareid when constructing vss.

--- crypto/vss/feldman_vss.go
+++ crypto/vss/feldman_vss.go
@@ -38,6 +38,23 @@ var (
 	zero = big.NewInt(0)
 	one  = big.NewInt(1)
 )
+// Check share ids of Shamir's Secret Sharing, return error if duplicate or 0 value found
+func CheckIndexes(ec elliptic.Curve, indexes []*big.Int) ([]*big.Int, error) {
+	visited := make(map[string]struct{})
+	for i, v := range indexes {
+		vMod := new(big.Int).Mod(v, ec.Params().N)
+		if vMod.Cmp(zero) == 0 {
+			return nil, errors.New("party index should not be 0")
+		}
+		vModStr := vMod.String()
+		if _, ok := visited[vModStr]; ok {
+			return nil, fmt.Errorf("duplicate indexes %s", vModStr)
+		}
+		visited[vModStr] = struct{}{}
+		indexes[i] = vMod
+	}
+	return indexes, nil
+}
 
 // Returns a new array of secret shares created by Shamir's Secret Sharing Algorithm,
 // requiring a minimum number of shares to recreate, of length shares, from the input secret
@@ -49,6 +66,11 @@ func Create(ec elliptic.Curve, threshold int, secret *big.Int, indexes []*big.In
 	if threshold < 1 {
 		return nil, nil, errors.New("vss threshold < 1")
 	}
+	ids, err := CheckIndexes(ec, indexes)
+	if err != nil {
+		return nil, nil, err
+	}
+
 	num := len(indexes)
 	if num < threshold {
 		return nil, nil, ErrNumSharesBelowThreshold
@@ -66,8 +88,8 @@ func Create(ec elliptic.Curve, threshold int, secret *big.Int, indexes []*big.In
 		if indexes[i].Cmp(big.NewInt(0)) == 0 {
 			return nil, nil, fmt.Errorf("party index should not be 0")
 		}
-		share := evaluatePolynomial(ec, threshold, poly, indexes[i])
-		shares[i] = &Share{Threshold: threshold, ID: indexes[i], Share: share}
+		share := evaluatePolynomial(ec, threshold, poly, ids[i])
+		shares[i] = &Share{Threshold: threshold, ID: ids[i], Share: share}
 	}
 	return v, shares, nil
 }
