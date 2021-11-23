697e6ece3c9381076abc341cb8c75e2d10682fde
creamwhip <dev3@swingby.network>
2021-11-23 19:38:14 +0800

    fix calls to BaseUpdate

--- eddsa/keygen/local_party.go
+++ eddsa/keygen/local_party.go
@@ -89,7 +89,7 @@ func (p *LocalParty) Start() *tss.Error {
 }
 
 func (p *LocalParty) Update(msg tss.ParsedMessage) (ok bool, err *tss.Error) {
-	return tss.BaseUpdate(p, msg, TaskName)
+	return tss.BaseUpdate(p, msg, TaskName, 0)
 }
 
 func (p *LocalParty) UpdateFromBytes(wireBytes []byte, from *tss.PartyID, isBroadcast bool) (bool, *tss.Error) {
